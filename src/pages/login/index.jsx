9febe02a290fc33a1fef207fe78f02af9c8e57cf
jayden <757970051@qq.com>
2022-08-17 17:54:35 +0800

    test

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,50 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // 引入ant组件
-import { Form, Input, Button, Menu } from 'antd'
+import { Form, Input, Button, Menu } from "antd";
 
-import './index.css'
+import "./index.css";
 // 首先完成布局
 // 然后再实现逻辑
 
 const Login = (porps) => {
-    // state 声明
-    const [selectdKey, setSelectedKey] = useState(['mail']) // 默认选中菜单
+  // state 声明
+  const [selectdKey, setSelectedKey] = useState(["mail"]); // 默认选中菜单
 
-    function changeSelectedKey(params) { // 改变菜单默认选中值
-        console.log(params)
-        setSelectedKey([params.key])
-    }
-    return (
-        <div className='login'>
-            <div className='logincont'>
-                <Menu mode="horizontal" className='login-menu' onClick={changeSelectedKey} selectedKeys={selectdKey} defaultSelectedKeys={['mail']}>
-                    <Menu.Item key="mail">
-                        邮箱
-                    </Menu.Item>
-                    <Menu.Item key="phone">
-                        手机号
-                    </Menu.Item>
-                </Menu>
-                <Form className='login-form'
-                    labelCol={{ span: 4 }}
-                    wrapperCol={{ span: 16 }}
-                >
-                    <div className='login-title'>
-                        <h2>手机号登录</h2>
-                    </div>
-                    {/*手机号 */}
-                    <Form.Item label='手机号'>
-                        <Input placeholder='请输入手机号' />
-                    </Form.Item>
-                    {/*密码 */}
-                    <Form.Item label='密码'>
-                        <Input type="password" placeholder='请输入密码' />
-                    </Form.Item>
-                </Form>
-            </div>
-        </div>
-    )
-}
+  function changeSelectedKey(params) {
+    // 改变菜单默认选中值
+    console.log(params);
+    setSelectedKey([params.key]);
+  }
+  return (
+    <div className="login">
+      <div className="logincont">
+        <Menu
+          mode="horizontal"
+          className="login-menu"
+          onClick={changeSelectedKey}
+          selectedKeys={selectdKey}
+          defaultSelectedKeys={["phone"]}
+        >
+          <Menu.Item key="mail">邮箱</Menu.Item>
+          <Menu.Item key="phone">手机号</Menu.Item>
+        </Menu>
+        <Form
+          className="login-form"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+        >
+          <div className="login-title">
+            <h2>手机号登录</h2>
+          </div>
+          {/*手机号 */}
+          <Form.Item label="手机号">
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+          {/*密码 */}
+          <Form.Item label="密码">
+            <Input type="password" placeholder="请输入密码" />
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
