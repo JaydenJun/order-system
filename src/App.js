d7258fd63ed8fbd1f954ea8d3c949ebecba30418
jayden <757970051@qq.com>
2022-08-17 16:40:40 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import 'antd/dist/antd.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Login from './pages/login/index.jsx'
+import { Spin } from 'antd';
+import './App.css';
+
+const App = function () {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Spin size="large" className="loading" spinning={false} />
+        <Switch>
+          <Redirect exact={true} from="/" to="/login"></Redirect>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
+export default App
+
